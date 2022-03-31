ccbef6498fd9c5ee34ccada6c91efc2a32c9a15e
Aritz Elgezabal <aelguezabal010@gmail.com>
2022-03-31 12:39:21 +0200

    Update Soluciones.R

--- 2. Modificaciones/Soluciones.R
+++ 2. Modificaciones/Soluciones.R
@@ -1,44 +1,114 @@
-# Limpiamos el directorio de trabajo
+#0. Limpiamos el espacio de trabajo
+###################################
+
 rm(list=ls())
 
-# Cargamos los datos.
-datos=read.csv("train.csv")
+# 1. Cargar los datos de Train
+##############################
 
-# Covierto en texto
-datos$MSZoning = as.character(datos$MSZoning)    
+#Cargar el CSV.
+datos = read.csv("train.csv")
 
-# Realizamos el cambio.
-datos$MSZoning = gsub("(all)", "", datos$MSZoning)  
-datos$MSZoning = as.factor(datos$MSZoning)
-levels(datos$MSZoning)
+# 2. Para la variable "MSZoning" eliminar de la variable
+#"C (all)" el " (all)" (sustituirlo por "")
+########################################################
 
-# Utilizando las "\\" realizamos la correcion final. 
-datos$MSZoning = as.character(datos$MSZoning) 
-datos$MSZoning = gsub("\\()", "", datos$MSZoning)  
-datos$MSZoning = as.factor(datos$MSZoning)
-levels(datos$MSZoning)
+datos$MSZoning = gsub("\\(all)", "", datos$MSZoning)
 
-datos$MSZoning = as.character(datos$MSZoning) 
-datos$MSZoning = gsub(" ", "", datos$MSZoning)  
-datos$MSZoning = as.factor(datos$MSZoning)
-levels(datos$MSZoning)
+# 3. Comprobar que se ha realizado correctamente el cambio
+##########################################################
 
+table(datos$MSZoning)
 
-# Creamos una nueva variable llamada crisis
-datos$Crisis="XXX"
+# 4. En caso de que no se haya realizado correctamente solucionarlo
+###################################################################
+# 5. Crear una nueva variable que se llame Crisis
+#################################################
 
-# Transformamos la variable.
-datos$Crisis[datos$YrSold<2008]="No"
-datos$Crisis[datos$YrSold>=2008]="Si"
-
-# Comprobamos.
+datos$Crisis = "XXX"
 table(datos$Crisis)
 
-CasasCaras = datos[order(datos$SalePrice,decreasing=TRUE)[1:10],]
+# 6. Transformar la variable anterior para determinar
+# (Debe tomar los valores Sí y No),basándose en la
+# variable "YrSold", si la casa se vendió en el 
+# periodo precrisis (antes de 2008) o durante la crisis (a partir de 2008)
+##########################################################################
+
+datos$Crisis[datos$YrSold<2008] = "No"
+datos$Crisis[datos$YrSold>=2008] = "Sí"
+
+# 7. Comprobar que se ha realizado correctamente
+################################################
+
+table(datos$Crisis)
+
+# 8. Crear un nuevo grupo de datos con las 20 casas más baratas,
+# en función de la variable "SalePrice".
+###############################################################
+
+#   - Ordenar el dataframe "datos" según la variable 'SalePrice'
+#   - Modo creciente                --> decreasing=FALSE
+#   - 20 casas por lo que generamos --> [1:20]
+datosCasasBaratas = datos[order(datos$SalePrice,decreasing=FALSE)[1:20],]
+
+# 9. Crear una nueva variable y clasificar las observaciones en Grande,
+#Mediano y Pequeño según la variable LotArea.
+#######################################################################
+
+# Crear la nueva variable
+datos$clasificacion = "XXX"
+# Mayores o iguales a 9000 = Grandes
+datos$clasificacion[datos$LotArea>=9000] = "Grandes"
+# Entre 5000 y 9000 = Medianos
+datos$clasificacion[datos$LotArea>5000 & datos$LotArea<9000] = "Medianos"
+# Menores o iguales a 5000 = Pequeños
+datos$clasificacion[datos$LotArea<=5000] = "Pequenos"
+
+# 10. ¿Cuántas casas hay de cada?
+#################################
+
+datosGrandes  = datos[datos$clasificacion=="Grandes",]
+datosMedianos = datos[datos$clasificacion=="Medianos",]
+datosPequenos = datos[datos$clasificacion=="Pequenos",]
+
+nrow(datosGrandes)
+nrow(datosMedianos)
+nrow(datosPequenos)
+
+# 11. ¿Cual es la media de la variable LotArea para cada una
+# de las categorías antes creadas (grande, mediano, pequeño)?
+############################################################
+
+mean(datos$LotArea[datos$clasificacion=="Grandes"])
+mean(datos$LotArea[datos$clasificacion=="Medianos"])
+mean(datos$LotArea[datos$clasificacion=="Pequenos"])
+
+# 12. ¿Y el sumatorio?
+######################
+
+sum(datos$LotArea[datos$clasificacion=="Grandes"])
+sum(datos$LotArea[datos$clasificacion=="Medianos"])
+sum(datos$LotArea[datos$clasificacion=="Pequenos"])
+
+# 13. Crear un nuevo grupo de datos con las 10 casas más caras,
+# en función de la variable "SalePrice".
+###############################################################
+
+datosCasasMasCaras = datos[order(datos$SalePrice,decreasing=TRUE)[1:10],]
+
+# 14. Determinar la calle en la que se encuentra la casa más cara
+#################################################################
 
 datos$Street[which.max(datos$SalePrice)]
+datosCasasMasCaras$Street[which.max(datosCasasMasCaras$SalePrice)]
 
-datos$Reforma=datos$YrSold-datos$YearRemodAdd
+# 10. Crear una variable llamada "Reforma" y que muestre los años
+# que pasaron entre la última reforma y la venta (YrSold-YearRemodAdd)
+######################################################################
 
+datos$Reforma = datos$YrSold - datos$YearRemodAdd
 
-write.csv(datos,"trainmod.csv", row.names=FALSE)
+# 11. Guardar los datos, write.csv(datos, "trainmod.csv", row.names=FALSE)
+##########################################################################
+
+write.csv(datos, "trainmod.csv", row.names=FALSE)
