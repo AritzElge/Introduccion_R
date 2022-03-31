d951523098793a3eac5bf00efc6bc2def0a33b9c
Aritz Elgezabal <aelguezabal010@gmail.com>
2022-03-31 12:42:17 +0200

    Update Soluciones.R

--- 4. Fechas/Soluciones.R
+++ 4. Fechas/Soluciones.R
@@ -1,62 +1,118 @@
-# Limpiamos el directorio de trabajo.
+# 0. Primero borramos las variables:
+#################################
+
 rm(list=ls())
 
-# Cargamos los datos.
-datos=read.csv("Ejemplo_4.csv",sep=";")
+# 1. Cargar los datos
+#####################
 
-# Mostramos los datos
+datos <- read.csv("Ejemplo_4.csv",header = TRUE,sep = ";")
+
+# 2. Mostrar los datos
+######################
+
 show(datos)
 
-# Realizamos un resumen estadistico de los datos.
+# 3. Realizar un resumen estadístico de los datos
+#################################################
+
 summary(datos)
 
-#Comprobamos el tipo de dato de la fecha.
-class(datos$CET)
+# 4. Comprobar si la fecha está en un formato correcto
+######################################################
 
-# Lo transformamos en formato fecha.
-datos$CET <- as.POSIXct(as.character(datos$CET), format="%d/%m/%Y")
+sapply(datos, class)
 
-# Comprobamos que el cambio se ha realizado correctamente.
-class(datos$CET)
+# 5. En caso de que la fecha no esté correcta transformarla al formato correcto
+###############################################################################
 
-# Extraemos el a.
-library(lubridate)
-datos$ano <- year(datos$CET)
+datos$CET <- as.Date(as.character(datos$CET), format="%d/%m/%Y")
 
-# Extraemos el mes.
-datos$mes <- month(datos$CET)
+# 6. Comprobar que la transformación se ha hecho de forma correcta
+##################################################################
 
-# Extraemos el dia.
-datos$dia <- day(datos$CET)
+sapply(datos, class)
 
-# Dibujamos la temperatura maxima en funcion del a.
-plot(datos$ano,datos$Max.TemperatureC)
+# 7. Crear una nueva variable que extraiga el año de la fecha
+#############################################################
 
-# Dibujamos la temperatura maxima en funcion del mes.
-plot(datos$mes,datos$Max.TemperatureC)
+#Primer se obtiene la fecha:
+date <-  as.Date(datos$CET,'%d-%m-%Y')
+date
 
-# Dibujamos la temperatu maxima en funcion del dia.
-plot(datos$dia,datos$Max.TemperatureC)
+#Después se separa el año de la fecha:
+fecha_año <- as.numeric(format(date,'%Y'))
+fecha_año
 
+# 8. Crear una nueva variable que extraiga el mes de la fecha
+#############################################################
 
-# Creamos un subconjunto de datos con julio y agosto.
-Verano=datos[datos$mes==7 | datos$mes==8,]
+fecha_mes <- as.numeric(format(date, '%m'))
+fecha_mes
 
-# Graficamos en funcion del a.
-plot(Verano$ano,Verano$Max.TemperatureC)
+# 9. Crear una nueva variable que extraiga el día de la fecha
+#############################################################
 
+fecha_dia <- as.numeric(format(date, '%d'))
+fecha_dia
 
-# Extraemos el dia de la semana.
-datos$diasemana = weekdays(as.Date(datos$CET))
+# 10. Realizar un gráfico de la temperatura máxima en función 
+# de los años y ver si existen diferencias a lo largo de los años
+#################################################################
 
-# Separamos el sabado y el domingo.
-datos$finde = 0
-datos$finde[datos$diasemana == "s畸ado" | datos$diasemana == "domingo"]=1
+plot(fecha_año, datos$Max.TemperatureC)
 
-#Graficamos en funcion del dia de la semana.
-plot(datos$finde,datos$Max.TemperatureC)
+# 11. Realizar un gráfico de la temperatura máxima en función de 
+# los meses y analizar si existen diferencias entre los meses
+################################################################
 
-# Calculamos la media del finde
-datos$Max.TemperatureC=as.numeric(datos$Max.TemperatureC)
-mediafinde=mean(datos$Max.TemperatureC[datos$finde==1],na.rm=TRUE)
-mediasemana=mean(datos$Max.TemperatureC[datos$finde==0],na.rm=TRUE)
+plot(fecha_mes, datos$Max.TemperatureC)
+
+# 12. Realizar un gráfico de la temperatura máxima en función de los
+# días y ver si existen diferencias según el día
+####################################################################
+
+plot(fecha_dia, datos$Max.TemperatureC)
+
+# 13. Extraer el día de la semana
+#################################
+
+datos$dia_semana <- weekdays(as.Date(datos$CET))
+
+# 14. Crear una variable que determine si se está en fin de semana o no
+#######################################################################
+
+datos$finDeSemana = "Sí"
+datos$finDeSemana[datos$dia_semana == "sábado"] = "No"
+datos$finDeSemana[datos$dia_semana == "domingo"] = "No"
+
+# 15. Realizar un gráfico en de la temperatura máxima en función de si
+# se está en fin de semana o no
+######################################################################
+
+datos$finDeSemana = as.factor(datos$finDeSemana)
+sapply(datos, class)
+
+plot(datos$finDeSemana,datos$Max.TemperatureC)
+
+# 16. Calcular la media de la temperatura máxima los fines de semana y 
+# entre semana
+######################################################################
+
+#Se crea un vector con sólo los datos de temperatura maxima en finde
+datosFinde <- datos$Max.TemperatureC[datos$finDeSemana == "Sí"]
+
+# Se comprueba que el número de datos en datosFinde concuerda con el numero SI:
+table(datos$finDeSemana)
+
+# Se calcula la media haciendo mean
+mean(datosFinde, na.rm = TRUE)
+
+#Se crea otro vector con sólo los datos de temp. maxima fuera de finde
+datosEntreSemana = datos$Max.TemperatureC[datos$finDeSemana == "No"]
+# Se comprueba el numero de datos
+
+table(datos$finDeSemana)
+# Se calcula la media
+
+mean(datosEntreSemana, na.rm = TRUE)
