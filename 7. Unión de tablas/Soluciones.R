0a3173fd2dc17538ebdf9ad43002927323ab4f26
Aritz Elgezabal <aelguezabal010@gmail.com>
2022-03-31 13:00:39 +0200

    Create Soluciones.R

---
+++ 7. Unión de tablas/Soluciones.R
@@ -0,0 +1,48 @@
+# 0. Limpiamos el espacio de trabajo
+####################################
+
+rm(list=ls())
+
+# 1. Cargar los datos "SalePrice1", "SalePrice2", "SalePrice3"
+##############################################################
+
+# 2. Unir las tres tablas "pegando" una a continuación de otra
+##############################################################
+
+# 3. Cargar los datos de “CasasCaras”
+#####################################
+
+# 4. Enriquecer la tabla “CasasCaras” con las variables incluidas en datos1, 
+# datos2 y datos3, utilizando dos fórmulas diferentes.
+############################################################################
+
+# 5. Cargar datosH
+##################
+
+# 6. Realizar las siguientes uniones con dos funciones:
+#######################################################
+
+#     a.  Enriquecer datos H con la información de “SalePrice1”,
+#         “SalePrice2”, “SalePrice3”.
+################################################################
+
+#     b.  Recogiendo toda la información recogida en todas las tablas
+#####################################################################
+
+#     c.  Recogiendo los elementos comunes.
+###########################################
+
+# 7. Seleccionar:
+#################
+
+#     a.  Los elementos comunes entre datosH y Casascaras
+##########################################################
+
+#     b.  Enriquecer datosH con Casascaras
+##########################################
+
+#     c.  Enriquecer Casascaras con datos H
+###########################################
+
+#     d.  Todos los elementos
+#############################
